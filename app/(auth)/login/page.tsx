71543d0b0118ca498a1bc79b9df2f778cf5ce8c6
Bridger Tower <bridgertower@gmail.com>
2026-09-01 10:55:40 -0600

    Remove database outage notice

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -19,14 +19,6 @@ export default async function LoginPage() {
           height={72}
         ></Image>
         <p className="text-center text-xl">Welcome to router.so</p>
-        <div className="rounded-md border border-yellow-500 border-dashed">
-          <p className="text-xs text-yellow-500 max-w-xs p-4">
-            On 6/19/25, our account with an upstream database provider was
-            deleted, losing past data. If you are on a paid plan, or have any
-            concerns, please reach out to 9d8dev@gmail.com. We are sincerely
-            sorry for the inconvenience.
-          </p>
-        </div>
         <p className="text-center text-muted-foreground">
           Login to access your account.
         </p>
